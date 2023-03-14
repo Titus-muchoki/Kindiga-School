dcb6655cbc0b4c04a496cc88b735bdee77c1d01e
kajela <titoyut56@gmail.com>
2023-03-14 04:31:24 -0400

    add the initial files

---
+++ src/main/java/dao/Sql2oStudentDao.java
@@ -0,0 +1,57 @@
+package dao;
+
+import models.Student;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class Sql2oStudentDao implements StudentDao{
+    public Sql2o sql2o;
+    public Sql2oStudentDao(Sql2o sql2o){
+        this.sql2o= sql2o;
+    }
+
+
+    @Override
+    public void add(Student student) {
+        String sql = "INSERT INTO students ( name, phonenummber, email)VALUES(:name, :phoneNumber, :email)";
+        try(Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(student)
+                    .executeUpdate()
+                    .getKey();
+            student.setId(id);
+        } catch (Exception ex){
+            System.out.println(ex);
+        }
+
+
+    }
+
+    @Override
+    public List<Student> getAll() {
+        return null;
+    }
+
+    @Override
+    public Student findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(int id, String name, String phoneNumber, String email) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
+}
